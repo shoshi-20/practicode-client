6171c768be9c8fb3749015a1e38f81090629e733
shoshi-20 <shos61926@gmail.com>
2023-04-16 15:13:02 +0300

    first commit

---
+++ src/service.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+axios.defaults.baseURL  = "http://localhost:5107/todoitems"
+axios.interceptors.response.use(function (response) {
+  console.log(response)
+  return response;
+}, function (error) {
+  console.log(error)
+  return Promise.reject(error);
+});
+export default {
+  getTasks: async () => {
+    const result = await axios.get()    
+    return result.data;
+  },
+
+  getTasksById: async (id) => {
+    const result = await axios.get(`/${id}`)    
+    return result.data;
+  },
+
+  addTask: async(newTodo)=>{
+    const result=axios.post('',{name:newTodo,isComplete:false,id:0})
+    console.log('addTask')
+    return result.data;
+  },
+
+  setCompleted: async(id, isComplete)=>{
+    // const task = await axios.get(`${apiUrl}/todoitems/${id}`)  
+    // task.iscomplete=isComplete;
+   const result= axios.put(`/${id}?isComplete=${isComplete}` )
+    console.log('setCompleted', {id, isComplete})
+    return result.data;
+  },
+
+  deleteTask:async(id)=>{
+    const result = await axios.delete(`/${id}`)
+    console.log('deleteTask')  
+    return result.data;
+    
+  }
+};
